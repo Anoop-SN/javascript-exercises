efb8fa0c1317c5d6d164e3d7b5ae343e56ecf4ec
Anoop S N <anoopsn57@gmail.com>
2026-02-27 12:31:44 +0530

    completed first 5 assignment of javascript foundations

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,6 +8,14 @@
 */
 
 // Add your code right below, good luck!
+let firstName = "Carlos";
+let lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+// let greeting = "Hello! My name is "+firstName+" "+lastName+" and I am "+(thisYear-birthYear)+" years old.";
+let fullName = firstName+" "+lastName;
+let age = thisYear-birthYear;
+let greeting = "Hello! My name is "+fullName+" and I am "+age+" years old.";
 
 
 
